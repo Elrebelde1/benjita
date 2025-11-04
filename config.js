89b8ceeb864838f7a98dcae0765f0267c90e7e98
Wichi <geraldorincon52@gmail.com>
2025-11-03 20:05:58 -0400

    Actualizar config.js

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ import axios from 'axios'
   [ '595972157130'],
   [ '174560573964411@lid', 'Rayo OFC', true ],
   [ '18498640873', "I'm Fz ~", true ],
-  [ '573113406369', 'bajobots', true ],
+  [ '584167231363', 'bajobots', true ],
   [ '51928626068', "MediaHub",true ],
   [ '172056423456949', true ]
   ]
